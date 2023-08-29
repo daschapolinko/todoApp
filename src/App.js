398ec4f019dd34dada4a282ea651991f6e93bbe0
daschapolinko <33525943+daschapolinko@users.noreply.github.com>
2023-08-29 18:51:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Footer from './components/Footer';
 function toggleProperty(arr, id, propName) {
   const idx = arr.findIndex((item) => item.id === id);
   const oldItem = arr[idx];
+  if (oldItem.timerId) {
+    clearInterval(oldItem.timerId);
+  }
   const value = !oldItem[propName];
   const item = { ...arr[idx], [propName]: value };
   return [...arr.slice(0, idx), item, ...arr.slice(idx + 1)];
